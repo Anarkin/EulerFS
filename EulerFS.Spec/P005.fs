cd34fdf25d5854c24ee38646b355434d4fbb9eaa
Adam Lengyel <lengyel.adam@gmail.com>
2020-04-07 21:50:10 +0200

    feat: add better p5 implementation

--- EulerFS.Spec/P005.fs
+++ EulerFS.Spec/P005.fs
@@ -3,10 +3,12 @@ namespace EulerFS
 [<Problem(005, Answer = 232792560)>]
 module ``Smallest multiple`` =
 
-    [<Solver>]
+  //[<Solver>]
     let impl () =
         seq { 2520 .. 20 .. System.Int32.MaxValue }
         |> Seq.filter (fun i -> not ([| 11 .. 20 |] |> Array.exists (fun i' -> i % i' <> 0)))
         |> Seq.head
 
-// todo factorization
+    [<Solver>]
+    let impl2 () =
+        Math.Lcm.naiveImpl [|1UL..20UL|]
