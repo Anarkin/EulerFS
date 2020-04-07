c7c4111542767819f647608ab09d332e554816ec
Adam Lengyel <lengyel.adam@gmail.com>
2020-04-07 20:48:55 +0200

    feat: add math.prime.factorization

---
+++ EulerFS.Spec/Math.Prime.Factorization.Spec.fs
@@ -0,0 +1,23 @@
+﻿namespace EulerFS.Math.Prime
+
+open NUnit.Framework
+
+module FactorizationSpec =
+
+    [<Test>]
+    [<TestCase(2UL, [| 2 |])>]
+    [<TestCase(3UL, [| 3 |])>]
+    [<TestCase(4UL, [| 2; 2 |])>]
+    [<TestCase(5UL, [| 5 |])>]
+    [<TestCase(6UL, [| 2; 3 |])>]
+    [<TestCase(7UL, [| 7 |])>]
+    [<TestCase(8UL, [| 2; 2; 2 |])>]
+    [<TestCase(9UL, [| 3; 3 |])>]
+    [<TestCase(10UL, [| 2; 5 |])>]
+    [<TestCase(360UL, [| 2; 2; 2; 3; 3; 5 |])>]
+    [<TestCase(2020UL, [| 2; 2; 5; 101 |])>]
+    let ``factorization`` (number, factors) =
+
+        let actualFactors = Factorization.naiveImpl number |> Array.ofSeq
+
+        Assert.AreEqual(factors, actualFactors)
---
+++ EulerFS.Spec/Math.Prime.Factorization.fs
@@ -0,0 +1,18 @@
+﻿namespace EulerFS.Math.Prime
+
+module Factorization =
+
+    let naiveImpl (number) =
+
+        let rec primeFactors (n, div) =
+            seq {
+                match n with
+                | n when n % div = 0UL ->
+                    yield div
+                    yield! primeFactors ((n / div), div)
+                | n when n > div -> yield! primeFactors (n, (div + 1UL))
+                | n when n > 1UL -> yield n
+                | _ -> ()
+            }
+
+        primeFactors (number, 2UL)
