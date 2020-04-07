800886cdec5932ff7f46ac09b980272acb2cb8af
Adam Lengyel <lengyel.adam@gmail.com>
2020-04-08 00:25:07 +0200

    refactor: math.lcm

--- EulerFS.Spec/Math.Lcm.fs
+++ EulerFS.Spec/Math.Lcm.fs
@@ -1,27 +1,28 @@
 namespace EulerFS.Math
 
-module Lcm = // lowest common multiple
+module Lcm =
 
     let naiveImpl (numbers) =
         numbers
 
         // get factors
-        |> Seq.map (fun i -> Prime.Factorization.naiveImpl i |> Seq.groupBy (fun prime -> prime))
+        |> Seq.map (fun n ->
+            Prime.Factorization.naiveImpl n
+            |> Seq.groupBy id
+            |> Seq.map (fun (factor, factors) -> (factor, factors |> Seq.length)))
 
-        // merge: keep factors with largest exponent only
-        |> Seq.fold (fun (acc: Map<uint64, int>) factorsWithExponents ->
-            let mutable result = acc
-            for (factor, exps) in factorsWithExponents do
-                let exp = exps |> Seq.length
-                if result.ContainsKey(factor) then
-                    if result.[factor] < exp then
-                        result <- result.Add(factor, exp)
-                else
-                    result <- result.Add(factor, exp)
-            result) Map.empty<uint64, int>
+        // keep factors with largest exponent only
+        |> Seq.fold (fun (map: Map<uint64, int>) factorsWithExponents ->
+            let mutable dict = map
+
+            for (factor, exp) in factorsWithExponents do
+                dict <-
+                    match dict.TryGetValue factor with
+                    | true, e when e < exp -> dict.Add(factor, exp)
+                    | false, _ -> dict.Add(factor, exp)
+                    | _ -> dict
+
+            dict) Map.empty<uint64, int>
 
         // multiply it all together
-        |> Map.fold (fun acc factor exponent ->
-            let exp = int32 exponent
-            acc * (pown factor exp)
-        ) 1UL
+        |> Map.fold (fun acc factor exponent -> acc * (pown factor exponent)) 1UL
