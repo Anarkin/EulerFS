80a54c90ba5812431a7e1257321e01017a174454
Adam Lengyel <lengyel.adam@gmail.com>
2020-04-07 20:49:37 +0200

    feat: add better p3 implementation

--- EulerFS.Spec/P003.fs
+++ EulerFS.Spec/P003.fs
@@ -3,13 +3,15 @@
 [<Problem(003, Answer = 6857)>]
 module ``Largest prime factor`` =
 
+    let input = 600851475143UL
+
     [<Solver>]
     let impl () =
-        let input = 600851475143UL;
-
         seq { 1UL..(input |> float |> sqrt |> uint64) }
         |> Seq.filter (fun i -> input % i = 0UL) // divisors
         |> Seq.filter (Math.Prime.Tester.naiveImpl)
         |> Seq.last
 
-// todo factorization
+    [<Solver>]
+    let impl2 () =
+        Math.Prime.Factorization.naiveImpl input |> Seq.last
