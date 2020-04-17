7500ef62d37f64d1500cea8a344ec3662a67c4aa
Adam Lengyel <lengyel.adam@gmail.com>
2020-04-18 00:58:30 +0200

    feat: solve p10 naively

---
+++ EulerFS.Spec/Solutions/P010.fs
@@ -0,0 +1,10 @@
+﻿namespace EulerFS
+
+[<Problem(010, Answer = 142913828922UL)>]
+module ``Summation of primes`` =
+
+    [<Solver>]
+    let impl() =
+        2UL + (seq { 3UL .. 2UL .. 2000000UL }
+        |> Seq.filter Math.Prime.Tester.naiveImpl
+        |> Seq.sum)
