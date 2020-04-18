96ba152879a8cbfc0cf2754ffcdda0cff4c6c462
Adam Lengyel <lengyel.adam@gmail.com>
2020-04-18 22:49:44 +0200

    feat: add prime sequence generator
    sieve of era

---
+++ EulerFS.Spec/Solutions/Math.Sequences.Prime.Spec.fs
@@ -0,0 +1,28 @@
+﻿namespace EulerFS.Math.Sequences
+
+open NUnit.Framework
+open System
+
+module PrimeSequenceSpecs =
+
+    [<Test>]
+    [<TestCase([| 2 |], 2)>]
+    [<TestCase([| 2; 3 |], 3)>]
+    [<TestCase([| 2; 3; 5; 7; 11 |], 11)>]
+    [<TestCase([| 2; 3; 5; 7; 11; 13; 17; 19; 23; 29 |], 29)>]
+    [<TestCase([| 2; 3; 5; 7; 11; 13; 17; 19; 23; 29 |], 30)>]
+    [<TestCase([| 2; 3; 5; 7; 11; 13; 17; 19; 23 |], 28)>]
+    let ``prime sequence`` (expected: int [], top: int) =
+        let actual = PrimeSequence.sieve (top) |> Seq.toArray
+
+        Assert.AreEqual(expected, actual)
+
+    [<Test>]
+    [<TestCase([| 2; 3; 5; 7; 11; 13; 17; 19; 23; 29 |], 500000)>]
+    let ``prime sequence finishes`` (expected: int [], top: int) =
+        let actual =
+            PrimeSequence.sieve (top)
+            |> Seq.take expected.Length
+            |> Seq.toArray
+
+        Assert.AreEqual(expected, actual)
---
+++ EulerFS.Spec/Solutions/Math.Sequences.Prime.fs
@@ -0,0 +1,31 @@
+﻿namespace EulerFS.Math.Sequences
+
+open System.Collections
+
+module PrimeSequence =
+
+    let sieve (top) = seq {
+
+     // let sieve = [| for _ in 0 .. top -> false |]
+        let sieve = BitArray(top + 1, false);
+
+        let evenNumbers = [|4 .. 2 .. top|]
+        for i in evenNumbers do
+            sieve.[i] <- true
+
+        yield 2
+        let mutable largest = 2
+
+        for i in 3 .. 2 .. top |> float |> sqrt |> int do
+            if not sieve.[i] then
+                yield i
+                largest <- i
+
+                for j in i * i .. i * 2 .. top do
+                    sieve.[j] <- true
+
+        for i in largest+1 .. top do
+            if not sieve.[i] then
+                yield i
+
+    }
