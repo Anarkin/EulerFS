c4f5507680d3f79f6a1b31ef9c88e88ec352612d
Adam Lengyel <lengyel.adam@gmail.com>
2020-04-18 23:06:45 +0200

    feat: add better implementation for p7

--- EulerFS.Spec/Solutions/P007.fs
+++ EulerFS.Spec/Solutions/P007.fs
@@ -1,11 +1,18 @@
 ﻿namespace EulerFS
+open System
 
 [<Problem(007, Answer = 104743)>]
 module ``10001st prime`` =
 
-    [<Solver>]
+  //[<Solver>]
     let impl () =
-        seq { 1UL .. System.UInt64.MaxValue }
+        seq { 1UL .. UInt64.MaxValue }
         |> Seq.filter (Math.Prime.Tester.naiveImpl)
+        |> Seq.take 10001
+        |> Seq.last
+
+    [<Solver>]
+    let impl2 () =
+        Math.Sequences.PrimeSequence.sieve 250000
         |> Seq.take 10001
         |> Seq.last
