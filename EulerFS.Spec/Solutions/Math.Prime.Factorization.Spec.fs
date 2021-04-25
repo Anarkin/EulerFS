ffc85f44a437ef400afd03cd3a8ba92a36f46f1b
Adam Lengyel <lengyel.adam@gmail.com>
2021-04-25 22:05:11 +0200

    refactor: prime factorization tests

--- EulerFS.Spec/Solutions/Math.Prime.Factorization.Spec.fs
+++ EulerFS.Spec/Solutions/Math.Prime.Factorization.Spec.fs
@@ -7,17 +7,19 @@ module FactorizationSpecs =
     [<Test>]
     [<TestCase(2UL, [| 2 |])>]
     [<TestCase(3UL, [| 3 |])>]
-    [<TestCase(4UL, [| 2; 2 |])>]
     [<TestCase(5UL, [| 5 |])>]
-    [<TestCase(6UL, [| 2; 3 |])>]
     [<TestCase(7UL, [| 7 |])>]
+    [<TestCase(1033UL, [| 1033 |])>]
+
+    [<TestCase(4UL, [| 2; 2 |])>]
+    [<TestCase(6UL, [| 2; 3 |])>]
     [<TestCase(8UL, [| 2; 2; 2 |])>]
     [<TestCase(9UL, [| 3; 3 |])>]
     [<TestCase(10UL, [| 2; 5 |])>]
     [<TestCase(360UL, [| 2; 2; 2; 3; 3; 5 |])>]
     [<TestCase(2020UL, [| 2; 2; 5; 101 |])>]
-    let ``factorization`` (number, factors) =
+    let ``factorization`` (number, primeFactors) =
 
         let actualFactors = Factorization.naiveImpl number |> Array.ofSeq
 
-        Assert.AreEqual(factors, actualFactors)
+        Assert.AreEqual(primeFactors, actualFactors)
