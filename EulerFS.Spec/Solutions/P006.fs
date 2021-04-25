49c9992f350cf09ac0b06d8bf6d16fa75ad8c65d
Adam Lengyel <lengyel.adam@gmail.com>
2021-04-25 22:19:46 +0200

    feat: add math solution for p6

--- EulerFS.Spec/Solutions/P006.fs
+++ EulerFS.Spec/Solutions/P006.fs
@@ -3,10 +3,18 @@
 [<Problem(006, Answer = 25164150)>]
 module ``Sum square difference`` =
 
-    [<Solver>]
+  //[<Solver>]
     let impl () =
         let (sumOfSquares, sum) =
             seq { 1 .. 100 }
             |> Seq.fold (fun (sumOfSquares, sum) current -> (sumOfSquares + current * current, sum + current)) (0, 0)
+
+        sum * sum - sumOfSquares
+
+    [<Solver>]
+    let impl2 () =
+        let n = 100
+        let sum = (n * (n+1)) / 2
+        let sumOfSquares = ( (n * (n+1)) * ((2 * n) + 1) ) / 6
 
         sum * sum - sumOfSquares
